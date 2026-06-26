c03d9c8b126040c6b692362232290979d41bda66
vasatitan24-create <vasatitan24@gmail.com>
2026-06-26 13:04:11 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/neirobitva/app/MainActivity.java
@@ -0,0 +1,49 @@
+package com.neirobitva.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("gonative://") || url.startsWith("median://")) {
+                    return true;
+                }
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.loadUrl("https://neiro-bitva.ru/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
